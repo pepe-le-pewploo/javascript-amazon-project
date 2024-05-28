1ab4068cb91e6f961dde4406c41a0052cd56c7d2
pepe-le-pewploo <mamun.muntasir003@gmail.com>
2024-05-29 00:33:03 +0600

    make payment summary interactive

--- scripts/checkout/orderSummary.js
+++ scripts/checkout/orderSummary.js
@@ -5,9 +5,6 @@ import dayjs from "https://unpkg.com/dayjs@1.11.10/esm/index.js";
 import {deliveryOptions, getDeliveryOption} from '../../data/deliveryOptions.js'
 import { renderPaymentSummary } from "./paymentSummary.js";
 
-const today = dayjs();
-const deliveryDate = today.add(7, 'days');
-console.log(deliveryDate.format('dddd, MMMM D'));
 
 export function renderOrderSummary() {
 
@@ -132,6 +129,8 @@ export function renderOrderSummary() {
         );
 
         container.remove();
+
+        renderPaymentSummary();
       })
     })
 
